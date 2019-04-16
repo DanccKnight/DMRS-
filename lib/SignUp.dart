3fec6903e27ca7d8be60098c9da5ccf6be1096d5
Ansuman Acharya <nitinansu12@gmail.com>
2019-04-16 23:45:05 +0530

    Added required dependencies for firestore

---
+++ lib/SignUp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email,_password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
